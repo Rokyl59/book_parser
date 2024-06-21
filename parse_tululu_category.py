31f63f50a702fb6c4ffeedb8ca4d597a58f79d32
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-06-21 08:34:30 +0500

    Отложил перехват исключения

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,7 +1,7 @@
 import time
 import requests
 from bs4 import BeautifulSoup
-from requests.exceptions import HTTPError, RequestException
+from requests.exceptions import HTTPError, RequestException, ConnectionError
 from functions_parser import check_for_redirect, TululuRedirectError
 
 
@@ -9,39 +9,40 @@ base_url = 'http://tululu.org/l55/'
 
 
 def get_book_ids_from_page(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response)
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        book_cards = soup.find_all('div', class_='bookimage')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    book_cards = soup.find_all('div', class_='bookimage')
 
-        book_ids = []
-        for book_card in book_cards:
-            book_link = book_card.find('a')['href']
-            book_id = book_link.split('/b')[-1].split('/')[0]
-            book_ids.append(int(book_id))
+    book_ids = []
+    for book_card in book_cards:
+        book_link = book_card.find('a')['href']
+        book_id = book_link.split('/b')[-1].split('/')[0]
+        book_ids.append(int(book_id))
 
-        return book_ids
-    except (HTTPError, RequestException) as error:
-        print(f'An error occurred while downloading book {book_id}: {error}')
-
-    except ConnectionError as error:
-        print(f'Connection error occurred while downloading book {book_id}: {error}')
-        print('Retrying in 5 seconds...')
-        time.sleep(5)
+    return book_ids
 
 
 def get_book_ids(start_page, end_page):
     all_book_ids = []
     for page in range(start_page, end_page + 1):
         url = f'{base_url}{page}/'
-        try:
-            book_ids = get_book_ids_from_page(url)
-            all_book_ids.extend(book_ids)
-        except TululuRedirectError as e:
-            print(f"Error: {e}")
-            continue
+        while True:
+            try:
+                book_ids = get_book_ids_from_page(url)
+                all_book_ids.extend(book_ids)
+                break
+            except (HTTPError, RequestException) as error:
+                print(f'An error occurred while downloading book IDs from page {page}: {error}')
+                break
+            except ConnectionError as error:
+                print(f'Connection error occurred while downloading book IDs from page {page}: {error}')
+                print('Retrying in 5 seconds...')
+                time.sleep(5)
+            except TululuRedirectError as e:
+                print(f"Error: {e}")
+                break
 
     return all_book_ids
